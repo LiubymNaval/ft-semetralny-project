0b0854d5723553ede525ce556c47b309abb5e2f0
LiubymNaval <navallubim@gmail.com>
2025-01-13 04:24:17 +0100

    Commit3
    Pridanie funkcií profilu, registrácie a prihlásenia do profilu

--- vue-project-semestralny-projekt/src/router/index.ts
+++ vue-project-semestralny-projekt/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfilView from '../views/ProfilView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'Domov',
       component: HomeView,
+    },
+    {
+      path: '/profil',
+      name: 'profil',
+      component: ProfilView,
     },
     {
       path: '/about',
